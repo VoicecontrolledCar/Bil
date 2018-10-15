d7213fd70fea1fe39ac66601a844313dba6907fe
Simon-Fredrik <simonfredrik2000@gmail.com>
2018-10-15 14:40:18 +0200

    Fixat felaktigheter i kodstrukturen och lagt till kommentarer

--- Real Shit/Carprogramming/Carprogramming.ino
+++ Real Shit/Carprogramming/Carprogramming.ino
@@ -1,23 +1,27 @@
 #include <Servo.h>
-int rightEngine;
-int leftEngine;
-int microServo;
-int left;
-int right;
-int straight;
+const int rightEngine; //Den analoga pin nummret som den sitter på
+const int leftEngine; //Den analoga pin nummret som den sitter på
+const int rightBackEngine;
+const int leftBackEngine;
+const int microServo; //Den analoga pin nummret som den sitter på
+const int left; //Vinkeln för servon för att svänga vänster
+const int right; //Vinkeln för servon för att svänga höger
+const int straight; //Vinkeln för servon för att vara rak
 int fileNumber;
 int fileValue;
 int import=0;
 
 Servo servo;
-int velocity(int a, int b);
-int turning(int a, int b);
+int velocity(int a, int b); //Declarar funktioner som kommer användas sen
+int turning(int a, int l, int r);
 
 void setup() {
  
   servo.attach (microServo) ;
   pinMode (rightEngine,OUTPUT);
   pinMode (leftEngine, OUTPUT);
+  pinMode (rightBackEngine, OUTPUT);
+  pinMode (leftBackEngine, OUTPUT);
   Serial.begin(9600);
 
 }
@@ -26,32 +30,32 @@ void loop() {
 if (Serial.available()>0){
   import = Serial.read();
 }
-if(import==0){
+if(import==0){ //"Stanna"
 analogWrite(rightEngine,0);
 analogWrite(leftEngine,0);
 servo.write(straight);
 }
-if(import==1){
+if(import==1){        //"Växel 1"
   int velocity(1,30);
   }
-if(import==2){
+if(import==2){        //"Växel 2"
   int velocity(2,60);
   }
-if(import==3){
+if(import==3){        //"Växel 3"
   int velocity(3,120);
   }
-if(import==4){
+if(import==4){        //"Växel 4"
   int velocity(4,255);
   }
-if(import==5){
-  int turning(left);
+if(import==5){        //"Sväng vänster"
+  int turning(left,30,0);
   }
-if(import==6){
-  int turning(right);
+if(import==6){        //"Sväng höger"
+  int turning(right,0,30);
   }
-if(import==7){}
+if(import==7){}       //Nästa kommando
 
-}  //loopen är slut
+}  //Loopen är slut
 
 
 
@@ -72,10 +76,11 @@ fileValue++;
 Serial.write(fileValue);  
   }
 
-  int turning(int a){
+  int turning(int a, int l, int r)
+  {
   fileValue=0;
-  analogWrite(leftEngine, 0);
-  analogWrite(rightEngine, 30);
+  analogWrite(leftEngine, l);
+  analogWrite(rightEngine, r);
   servo.write(a);
     while(Serial.available()==0){
     delay(1);
