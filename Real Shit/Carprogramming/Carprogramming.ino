3170645ccffb2c1a2902669a2000de8e6ceaf803
Oliver Larsson <olla16@nykopingsenskilda.se>
2018-10-15 16:10:30 +0200

    Gjorde herekommandot. La till lite variablar till herekommandot.

--- Real Shit/Carprogramming/Carprogramming.ino
+++ Real Shit/Carprogramming/Carprogramming.ino
@@ -7,13 +7,18 @@ const int microServo; //Den analoga pin nummret som den sitter på
 const int left; //Vinkeln för servon för att svänga vänster
 const int right; //Vinkeln för servon för att svänga höger
 const int straight; //Vinkeln för servon för att vara rak
-int fileNumber;
-int fileValue;
+int arrayNumber;
+int arrayValue;
 int import=0;
+int here[2][2000];
+int commandNumber=0;
+int x=-1;
+int y;
 
 Servo servo;
 int velocity(int a, int b); //Declarar funktioner som kommer användas sen
-int turning(int a, int l, int r);
+int turning(int a, int l, int r, int n);
+int Here();
 
 void setup() {
  
@@ -27,9 +32,10 @@ void setup() {
 }
 
 void loop() {
+if(x==-1){
 if (Serial.available()>0){
   import = Serial.read();
-}
+}}
 if(import==0){ //"Stanna"
 analogWrite(rightEngine,0);
 analogWrite(leftEngine,0);
@@ -48,13 +54,13 @@ if(import==4){        //"Växel 4"
   int velocity(4,255);
   }
 if(import==5){        //"Sväng vänster"
-  int turning(left,30,0);
+  int turning(left,30,0,6);
   }
 if(import==6){        //"Sväng höger"
-  int turning(right,0,30);
+  int turning(right,0,30,5);
   }
-if(import==7){}       //Nästa kommando
-
+if(import==7){}       //Here
+int Here;
 }  //Loopen är slut
 
 
@@ -63,28 +69,74 @@ if(import==7){}       //Nästa kommando
 
 int velocity(int a, int b)
 {
-  fileNumber=a;
-fileValue=0;
+  arrayNumber=a;
+arrayValue=0;
 servo.write(straight);
 analogWrite(rightEngine, b);
 analogWrite(leftEngine, b);
-Serial.write(fileNumber);
+if(x==-1)
+{
 while(Serial.available()==0){
 delay(1);
-fileValue++;
+arrayValue++;
 }
-Serial.write(fileValue);  
+here[0][commandNumber]=arrayNumber;  
+here[1][commandNumber]= arrayValue;
+commandNumber++;
+}
+else
+{
+  delay(y);
+  Here();
+}
   }
 
-  int turning(int a, int l, int r)
+  int turning(int a, int l, int r, int n)
   {
-  fileValue=0;
+  arrayNumber=n;
+  arrayValue=0;
   analogWrite(leftEngine, l);
   analogWrite(rightEngine, r);
   servo.write(a);
+   if(x==-1)
+   {
     while(Serial.available()==0){
     delay(1);
-    fileValue++;
+    arrayValue++;
     }
-    Serial.write(fileValue);
-  }  
+    here[0][commandNumber]=arrayNumber;
+    here[1][commandNumber]=arrayValue;
+    commandNumber++;
+   }
+   else{
+    delay(y);
+    Here();
+    }
+    }  
+
+  int Here()
+  {
+    //Vänd också
+    x++;
+    import=here[0][x];
+    y=here[1][x];
+    if(import=0){
+      x++;
+      analogWrite(rightEngine,0);
+      analogWrite(rightBackEngine,0);
+      analogWrite(leftEngine,0);
+      analogWrite(leftBackEngine,0);
+      servo.write(straight);
+      
+      for(int w=0;w++;w<x){
+        here[0][w]=0;
+        here[1][w]=0;
+      }
+      x=-1;
+      commandNumber=0;
+      
+      
+      }
+    
+    
+    }
