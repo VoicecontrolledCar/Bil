d77da6c5c6a72aaa34402d6c83c0a87b801b93fd
Simon-Fredrik <simonfredrik2000@gmail.com>
2018-10-15 22:52:05 +0200

    Jag har bytt namn på ett antal variablar så de är mer förståeliga, och har skapat funktionen STOPP och har gjort omstrukturering
    ArrayValue = durationWrite
    ArrayNumber = executorValue
    import = executer
    here [][] = memory [][]
    commandNumber = commandWriteNumber
    x = commandReadNumber
    y = durationRead

--- Real Shit/Carprogramming/Carprogramming.ino
+++ Real Shit/Carprogramming/Carprogramming.ino
@@ -1,142 +1,167 @@
 #include <Servo.h>
-const int rightEngine; //Den analoga pin nummret som den sitter på
-const int leftEngine; //Den analoga pin nummret som den sitter på
-const int rightBackEngine;
-const int leftBackEngine;
-const int microServo; //Den analoga pin nummret som den sitter på
-const int left; //Vinkeln för servon för att svänga vänster
-const int right; //Vinkeln för servon för att svänga höger
-const int straight; //Vinkeln för servon för att vara rak
-int arrayNumber;
-int arrayValue;
-int import=0;
-int here[2][2000];
-int commandNumber=0;
-int x=-1;
-int y;
-
 Servo servo;
-int velocity(int a, int b); //Declarar funktioner som kommer användas sen
-int turning(int a, int l, int r, int n);
+
+//Nummret på den analoga pin för de olika komponenterna
+const int rightEngine=10; 
+const int leftEngine=9; 
+const int rightReverse=6; 
+const int leftReverse=5; 
+const int microServo=3; 
+
+//Vinkeln för servon för de olika riktningarna den kan ha
+const int left=60; 
+const int right=120; 
+const int straight=90; 
+
+//Olika variablar deklareras, så de kan användas senare
+int executor=0;             //Variabeln vars värde styr bilen i loopen, gamla import
+int executorValue;          //Skriver in vilket kommando som använts i minnet
+int durationWrite;          //Skriver in hur länge ett kommando använts i minnet
+int memory[2][2000];        //En array som sparar bilens rörelser
+int commandWriteNumber=0;   //Kommer ihåg vilken kolumn som används för att skriva in i minnet
+int commandReadNumber=-1;   //Kommer ihåg vilken kolumn som används för att läsa ur minnet
+int durationRead=0;         //Läser ur minnet hur länge ett kommando använts i minnet, används för here
+
+//De olika funktioner och deras datatyper deklararas här
+int velocity(int gearNumber, int spead);
+int turning(int angle, int leftEngineSpead, int rightEngineSpead, int exportValue);
 int Here();
+int STOPP ();
+
 
 void setup() {
  
   servo.attach (microServo) ;
   pinMode (rightEngine,OUTPUT);
   pinMode (leftEngine, OUTPUT);
-  pinMode (rightBackEngine, OUTPUT);
-  pinMode (leftBackEngine, OUTPUT);
+  pinMode (rightReverse, OUTPUT);
+  pinMode (leftReverse, OUTPUT);
   Serial.begin(9600);
-
 }
 
+
 void loop() {
-if(x==-1){
+
+//ett test så att herefunktionen inte håller på innan executor får bluetoothsignalens värde
+if(commandReadNumber==-1){
 if (Serial.available()>0){
-  import = Serial.read();
+  executor = Serial.read();
 }}
-if(import==0){ //"Stanna"
+
+if(executor==0){        //"Stanna"
 analogWrite(rightEngine,0);
 analogWrite(leftEngine,0);
 servo.write(straight);
 }
-if(import==1){        //"Växel 1"
+if(executor==1){        //"Växel 1"
   int velocity(1,30);
   }
-if(import==2){        //"Växel 2"
+if(executor==2){        //"Växel 2"
   int velocity(2,60);
   }
-if(import==3){        //"Växel 3"
+if(executor==3){        //"Växel 3"
   int velocity(3,120);
   }
-if(import==4){        //"Växel 4"
+if(executor==4){        //"Växel 4"
   int velocity(4,255);
   }
-if(import==5){        //"Sväng vänster"
+if(executor==5){        //"Sväng vänster"
   int turning(left,30,0,6);
   }
-if(import==6){        //"Sväng höger"
+if(executor==6){        //"Sväng höger"
   int turning(right,0,30,5);
   }
-if(import==7){}       //Here
+if(executor==7){       //Here
 int Here;
 }  //Loopen är slut
-
-
-
-
-
-int velocity(int a, int b)
-{
-  arrayNumber=a;
-arrayValue=0;
-servo.write(straight);
-analogWrite(rightEngine, b);
-analogWrite(leftEngine, b);
-if(x==-1)
-{
-while(Serial.available()==0){
-delay(1);
-arrayValue++;
 }
-here[0][commandNumber]=arrayNumber;  
-here[1][commandNumber]= arrayValue;
-commandNumber++;
+
+
+
+
+int velocity(int gearNumber, int spead)
+{
+  executorValue=gearNumber;
+  durationWrite=0;
+  servo.write(straight);
+  analogWrite(rightEngine, spead);
+  analogWrite(leftEngine, spead);
+      if(commandReadNumber==-1)
+        {
+            while(Serial.available()==0){     //Borde det inte vara >=0 ???
+            delay(1);
+            durationWrite++;
+}
+  memory[0][commandWriteNumber]=executorValue;  
+  memory[1][commandWriteNumber]= durationWrite;
+  commandWriteNumber++;
 }
 else
 {
-  delay(y);
+  delay(durationRead);
   Here();
 }
   }
 
-  int turning(int a, int l, int r, int n)
+
+
+
+  int turning(int angle, int leftEngineSpead, int rightEngineSpead, int exportValue)
   {
-  arrayNumber=n;
-  arrayValue=0;
-  analogWrite(leftEngine, l);
-  analogWrite(rightEngine, r);
-  servo.write(a);
-   if(x==-1)
-   {
-    while(Serial.available()==0){
-    delay(1);
-    arrayValue++;
-    }
-    here[0][commandNumber]=arrayNumber;
-    here[1][commandNumber]=arrayValue;
-    commandNumber++;
+  executorValue=exportValue;
+  durationWrite=0;
+  analogWrite(leftEngine, leftEngineSpead);
+  analogWrite(rightEngine, rightEngineSpead);
+  servo.write(angle);
+     if(commandReadNumber==-1)
+        {
+         while(Serial.available()==0){
+         delay(1);
+          durationWrite++;
+         }
+    memory[0][commandWriteNumber]=executorValue;
+    memory[1][commandWriteNumber]=durationWrite;
+    commandWriteNumber++;
    }
    else{
-    delay(y);
+    delay(durationRead);
     Here();
     }
     }  
 
+
+
+
+    int STOPP ()
+    {
+      analogWrite(rightEngine,0);
+      analogWrite(rightReverse,0);
+      analogWrite(leftEngine,0);
+      analogWrite(leftReverse,0);
+      servo.write(straight); 
+    }
+
+
+
+
   int Here()
   {
-    //Vänd också
-    x++;
-    import=here[0][x];
-    y=here[1][x];
-    if(import=0){
-      x++;
-      analogWrite(rightEngine,0);
-      analogWrite(rightBackEngine,0);
-      analogWrite(leftEngine,0);
-      analogWrite(leftBackEngine,0);
-      servo.write(straight);
-      
-      for(int w=0;w++;w<x){
-        here[0][w]=0;
-        here[1][w]=0;
+    //Vänd också!!!
+    commandReadNumber++;
+    executor=memory[0][commandReadNumber];
+    durationRead=memory[1][commandReadNumber];
+    
+    if(executor=0){     //När det inte längre finns data att läsa in så stannar bilen
+     
+            STOPP ();
+            for(int w=0;w++;w<2000){     //Denna loop tömmer minnet
+              memory[0][w]=0;
+              memory[1][w]=0;
       }
-      x=-1;
-      commandNumber=0;
+     
+      commandWriteNumber=0;
+      commandReadNumber=-1;
       Serial.read();
-      
-      
       }
     
     
