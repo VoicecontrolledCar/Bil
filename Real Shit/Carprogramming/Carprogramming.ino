5b20c4282b8140a28461a8609260050ebc41447e
Simon-Fredrik <simonfredrik2000@gmail.com>
2019-01-29 16:29:07 +0100

    fixat nödstopp i here
    Fixat så att varje gång en rad har lästs i here så testar den om någon vill stanna bilen. Snyggade till några kommenatarer och la till några

--- Real Shit/Carprogramming/Carprogramming.ino
+++ Real Shit/Carprogramming/Carprogramming.ino
@@ -3,28 +3,27 @@
 //2: Kunna svänga medans vi backar.
 
 
-
 #include <Servo.h>  
 Servo servo;
-//Kom ihåg att lösa i koden att rightReverse är digital, vi får göra en egen pulsmodulering.
+
 //Nummret på den analoga pin för de olika komponenterna
 const int rightReverse=12; 
 const int leftEngine=3; 
-const int rightEngine=6; // Digital Pin, PIn 9 och 10 funkar ej med servobiblotek
+const int rightEngine=6; 
 const int leftReverse=13; 
-const int microServo=11; 
+const int microServo=11;      //  Pin 9 och 10 funkar ej med servobibloteket
 
-//Vinkeln för servon för de olika riktningarna och motorhastigheter
+//Vinkeln för servon för de olika riktningarna, motorhastigheter och andra konstanter
 const int left=45; 
 const int right=135; 
 const int straight=90; 
-const int turningMotorP = 230; //Testas
-const int notTurningMotorP = 0; //Testa
-const int ST = 500; //Testas - (stopp-tid (millisekunder))
-const int t1 = 1500;  
-const int t2 = 2000;
+const int turningMotorP = 230; 
+const int notTurningMotorP = 0;   
+const int ST = 500;               //Tiden för bilen att stanna i millisekunder
+const int t1 = 1500;              //Sväng-tiden i turnAroundfunktionen
+const int t2 = 2000;              //Back-tiden i turnAroundfunktionen
 
-//Variablar deklareras
+//Globala variablar deklareras
 int executor=0;             //Variabeln vars värde styr bilen i loopen     
 int durationWrite;          //Skriver in hur länge ett kommando använts i minnet, tiden ett kommando används
 int memory[2][200];         //En array som sparar bilens rörelser
@@ -34,12 +33,12 @@ int durationRead=0;         //Läser ur minnet hur länge ett kommando använts 
 
 //De olika funktioner och deras datatyper deklararas här
 int velocity(int executor, int power);
-int turning(int angle, int leftEngineSpead, int rightEngineSpead, int executor);
-void Here();
+int turning(int angle, int leftEngineSpeed, int rightEngineSpeed, int Executor);
+void Here ();
 void STOP ();
- void turnAround ();
+void turnAround ();
 void clearMemory ();
-void reverse(int Executor);
+void reverse (int Executor);
 
 
 
@@ -50,8 +49,9 @@ void setup() {             //Visar för arduinon hur pinsen ska användas och st
   pinMode (leftEngine, OUTPUT);
   pinMode (rightReverse, OUTPUT);
   pinMode (leftReverse, OUTPUT);
-  Serial.begin(9600);
-  STOP();
+  Serial.begin (9600);
+  STOP ();
+  clearMemory();
 }
 
 
@@ -67,42 +67,40 @@ if (Serial.available()>0){
 }}
 
 if(executor==0){        //"Stanna"
-  STOP();
+  STOP ();
 }
 if(executor==1){        //"Backa"
-  reverse(1);
+  reverse (1);
   }
 if(executor==2){        //"Växel 2"
-  velocity(2,150);
+  velocity (2, 150);
   }
 if(executor==3){        //"Växel 3"
-  velocity(3,200);
+  velocity (3, 200);
   }
 if(executor==4){        //"Växel 4"
-  velocity(4,255);
+  velocity (4, 255);
   }
 if(executor==5){        //"Sväng vänster"
-  turning(left,0,230,6);
+  turning (left, 0, 230, 6);
   }
 if(executor==6){        //"Sväng höger"
-  turning(right,230,0,5);
+  turning (right, 230, 0, 5);
   }
 if(executor==7){       //"Here"
-  Here();
+  Here ();
 }
 if (executor==8){      //"Vänd"
-  turnAround();
+  turnAround ();
 }
 if (executor==9) {     //"Nollställ minnet"
   clearMemory ();
 }
-//Loopen är slut
-}
+}//Loopen är slut
 
 
 
-
-int velocity(int executor, int power)
+int velocity (int executor, int power)
 {
   STOP();
   delay(100);
@@ -116,8 +114,8 @@ int velocity(int executor, int power)
             delay(1);
             durationWrite++;
 }
-  memory[0][commandWriteNumber]=executor;  
-  memory[1][commandWriteNumber]= durationWrite;
+  memory [0][commandWriteNumber] = executor;  
+  memory [1][commandWriteNumber] = durationWrite;
   commandWriteNumber++;
 }
 else
@@ -130,38 +128,38 @@ else
 
 
 
-  int turning(int angle, int leftEngineSpead, int rightEngineSpead, int executor)
+  int turning (int angle, int leftEngineSpeed, int rightEngineSpeed, int Executor)
   {
   durationWrite=0;
-  analogWrite(leftEngine, leftEngineSpead);
-  analogWrite(rightEngine, rightEngineSpead);
-  servo.write(angle);
+  analogWrite (leftEngine, leftEngineSpeed);
+  analogWrite (rightEngine, rightEngineSpeed);
+  servo.write (angle);
      if(commandReadNumber==-1)
         {
          while(Serial.available()<=0){    
-         delay(1);
+         delay (1);
           durationWrite++;
          }
-    memory[0][commandWriteNumber]=executor;
-    memory[1][commandWriteNumber]=durationWrite;
+    memory [0][commandWriteNumber] = Executor;
+    memory [1][commandWriteNumber] = durationWrite;
     commandWriteNumber++;
    }
    else{
-    delay(durationRead);
-    Here();
+    delay (durationRead);
+    Here ();
     }
     }  
 
 
 
 
-    void STOP ()  //Ska vi göra STOP lite smartare genom att matcha stop med att skicka ström till motsatt hål som den åker mot; (iaf framåt)?
+    void STOP () 
     {
-      analogWrite(rightEngine,0);
-      digitalWrite(rightReverse,LOW);
-      analogWrite(leftEngine,0);
-      analogWrite(leftReverse,0);
-      servo.write(straight); 
+      analogWrite (rightEngine, 0);
+      digitalWrite (rightReverse, LOW);
+      analogWrite (leftEngine, 0);
+      analogWrite (leftReverse, LOW);
+      servo.write (straight); 
     }
 
 
@@ -172,10 +170,16 @@ else
     commandReadNumber--;       //Gör värdet till -2 ifall det är första gången - annars vänder bilen igen
     if (commandReadNumber==-2) turnAround();
     commandReadNumber=commandReadNumber+2;
-    executor=memory[0][commandReadNumber];
+    executor=memory[0][commandReadNumber];        //Sätter executorn till minnets värde istället för inputens
     durationRead=memory[1][commandReadNumber];
     
-    if(executor==0){                   //När det inte längre finns data att läsa in så stannar bilen
+    if (Serial.available()>0){                    //Kollar så ingen gjort ett nödstopp.
+      int emergency = Serial.read();
+      emergency = emergency-48;
+      if (emergency == 0) executor = 0;
+    }
+    
+    if(executor == 0){                   //När det inte längre finns data att läsa in så stannar bilen
       STOP ();
       clearMemory ();
       
